3fdc0d13998994940002c3856287fc84d30b1869
Superpen-Dev <allitoheebishola1@gmail.com>
2025-01-19 23:48:18 +0100

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,8 @@
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
+// https://vite.dev/config/
 export default defineConfig({
-  base: "/project-portifolio/", // Ensure this matches your GitHub repo name
+  plugins: [react()],
+  base: "/new-portfolio",
 });
