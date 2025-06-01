225cc4f7f4ff5d8a832dd8ac92f2f5d087aedc51
Superpen-Dev <allitoheebishola1@gmail.com>
2025-06-01 23:55:00 +0100

    made some changes

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -1,40 +1,66 @@
-import React from "react";
 import "./Portfolio.css";
 
 // Import images
 import McanImage from "../assets/Mcan.jpg";
+import AlKogiwwyyImage from "../assets/AlKogiwwyy.png";
+import ChatBotImage from "../assets/ChatBot.png";
 import CatImage from "../assets/Cat.jpg";
 import SupercomImage from "../assets/Supercom.jpg";
 import ReliefImage from "../assets/Relief.jpg";
 import GameImage from "../assets/Game.jpg";
+
 
 const projects = [
   {
     title: "MCAN Project",
     image: McanImage,
     link: "https://superpen-dev.github.io/mcan-ondo-project/mcan-ondo-main/index.html",
+    type: "Static",
+  },
+  {
+    title: "Al-Kogiwwyy Casuals",
+    image: AlKogiwwyyImage,
+    link: "https://al-kogiwwyy-casual.vercel.app/",
+    type: "React",
+  },
+  {
+    title: "Superpen Chat Bot",
+    image: ChatBotImage,
+    link: "https://superpen-dev.github.io/superpen-chatbot/",
+    type: "React + Bootstrap",
   },
   {
     title: "Cat Facts",
     image: CatImage,
     link: "https://superpen-dev.github.io/cat-facts/",
+    type: "Static",
   },
   {
     title: "Supercom",
     image: SupercomImage,
     link: "https://superpen-dev.github.io/supercom/",
+    type: "Static",
   },
   {
     title: "Relief Academy",
     image: ReliefImage,
     link: "https://superpen-dev.github.io/reliefacademy/",
+    type: "Static",
   },
   {
     title: "Game Assignment",
     image: GameImage,
     link: "https://superpen-dev.github.io/Assignment-game/",
+    type: "Static",
   },
 ];
+
+// Tech label mapping
+const techLabels = {
+  "React": "React & Tailwind",
+  "React + Bootstrap": "React & Bootstrap",
+  "Static": "HTML, CSS & JS",
+};
 
 const Portfolio = () => {
   return (
@@ -47,7 +73,7 @@ const Portfolio = () => {
               <img src={project.image} alt={project.title} />
               <div className="overlay">
                 <p>{project.title}</p>
-                <span>Website Coding (HTML, CSS, JS)</span>
+                <span>{techLabels[project.type] || "Website Project"}</span>
               </div>
             </a>
           </div>
